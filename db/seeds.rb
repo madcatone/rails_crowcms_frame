8f454fbd24dda6c7518d48c64c35efb48c49abcc
hongjunhao <rubyinhell@gmail.com>
2016-05-26 15:36:03 +0800

    fix database.yml and db/seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-# User.create(id: 1, email: 'admin@163.com', password: 'admin1234')
+User.create(email: 'admin@163.com', password: 'admin1234')
 
 # Admin::Category.create(id: 1, title: '信息中心', is_article: false)
 # Admin::Category.create(id: 2, ancestry: 1, title: '新闻', is_article: true, is_file: true, is_abstract: true )
@@ -15,11 +15,11 @@
 # Admin::Category.create(id: 4, title: '产品中心', is_article: false)
 # 
 # 
-Admin::Menu.create(id: 1, name: '企业介绍', url: '/about-us')
-Admin::Menu.create(id: 2, name: '企业新闻', url: '/news')
-Admin::Menu.create(id: 3, name: '企业公告', url: '/notices')
-Admin::Menu.create(id: 4, name: '企业产品', url: '/products')
-Admin::Menu.create(id: 5, name: '联系我们', url: '/contract-us')
+Admin::Menu.create(name: '企业介绍', url: '/about-us')
+Admin::Menu.create(name: '企业新闻', url: '/news')
+Admin::Menu.create(name: '企业公告', url: '/notices')
+Admin::Menu.create(name: '企业产品', url: '/products')
+Admin::Menu.create(name: '联系我们', url: '/contract-us')
 # Admin::Page.create(id: 1, title: '公司介绍')
 # Admin::Page.create(id: 2, title: '联系我们')
 
